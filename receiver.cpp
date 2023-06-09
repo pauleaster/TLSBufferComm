47336b33df0327f69f45fb7a213812ac41491e9a
Paul Easter <paul.john.easter@gmail.com>
2023-06-09 17:47:25 +1000

    Simple receiver written

---
+++ receiver.cpp
@@ -0,0 +1,33 @@
+// Simple receiver program using boost asio library
+// C++ version 14
+
+#include <iostream>
+#include <array>
+#include <boost/asio.hpp>
+
+int main() {
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::acceptor acceptor(
+        io_service, boost::asio::ip::tcp::endpoint(
+            boost::asio::ip::tcp::v4(), 1234));
+    boost::asio::ip::tcp::socket socket(io_service);
+
+    // Receive data
+    std::array<char, 128> buffer;
+    boost::system::error_code error;
+    size_t bytesRead = socket.read_some(boost::asio::buffer(buffer), error);
+
+    if (error == boost::asio::error::eof) {
+        std::cout << "Connection closed by peer" << std::endl;
+    } else if (error) {
+        std::cout << "Error: " << error.message() << std::endl;
+    } else {
+        std::cout << "Received " << bytesRead << " bytes" << std::endl;
+        std::cout << "Message: " << std::string(buffer.data(), bytesRead) << std::endl;
+    }
+    
+    // Close the socket
+    socket.close();
+
+    return 0;
+}
